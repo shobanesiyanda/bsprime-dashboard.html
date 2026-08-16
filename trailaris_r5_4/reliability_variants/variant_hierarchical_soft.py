358ab3a539605ea15db7eae731f124a943c1a82a
shobanesiyanda <shobane.siyanda@gmail.com>
2026-08-16 15:44:10 +0200

    build: add soft hierarchical reliability precision variant

---
+++ trailaris_r5_4/reliability_variants/variant_hierarchical_soft.py
@@ -0,0 +1,4 @@
+import R5_BASE_CAUSAL_ENGINE as base
+from reliability_common import promote
+replay_r5=base.replay_r5
+def promote_for_week(cands,week_start):return promote(cands,week_start,'soft')
